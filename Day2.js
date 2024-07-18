c1edd56cd65d679ba36ebe69b3349d51c892ae83
VedanshPandey85 <202312085@daiict.ac.in>
2024-07-18 12:38:01 +0530

    Day 3

--- Day2.js
+++ Day2.js
@@ -15,6 +15,8 @@ const day2Learning = () => {
   //multiply
   console.log("subtract :- ", number1 * number2);
 
+  //assignment operators
+
   let x = 12;
   x += 20;
   console.log("new value for x is :- ", x);
@@ -22,6 +24,7 @@ const day2Learning = () => {
   x -= 20;
   console.log("new value for x is :- ", x);
 
+  //comparision
   let a = 21;
   let b = 10;
   let c = "10";
@@ -35,16 +38,20 @@ const day2Learning = () => {
   console.log("result for ==", b == c);
   console.log("result for ===", b === c);
 
+  //logical
+
   var value1 = 6;
-  var value2 = 12;
+  var value2 = 12; // it returns the second true value in this example it will return 12
   console.log("&&---", value1 && value2);
 
   var value3 = 6;
-  var value4 = 12;
+  var value4 = 12; // it returns the first true condition that will be 6 in this case
   console.log("||---", value3 || value4);
 
   var isMarried = false;
   console.log("!---", !isMarried);
+
+  //ternary oprtator
 
   let testNumber = -10;
   testNumber >= 0
---
+++ Day3.js
@@ -0,0 +1,114 @@
+const day3Learning = () => {
+  const negativePositive = (number) => {
+    //program to check number is positive , negative , or zero
+    if (number > 0) {
+      console.log("positive");
+    } else if (number == 0) {
+      console.log("zero");
+    } else {
+      console.log("negative");
+    }
+  };
+
+  const isEligibleToVote = (age) => {
+    if (age >= 18) {
+      console.log("Eligible to vote");
+    } else {
+      console.log("Not eligible to vote");
+    }
+  };
+
+  const findLargetst = () => {
+    const getRandomRumber = () => {
+      return Math.floor(Math.random() * 100);
+    };
+
+    let number1 = getRandomRumber();
+    let number2 = getRandomRumber();
+    let number3 = getRandomRumber();
+
+    console.log(number1, number2, number3);
+
+    if (number1 == number2 && number2 == number3) {
+      console.log("All three numbers are equal!!");
+    } else {
+      if (number1 > number2 && number1 > number3) {
+        console.log("number 1 biggest!");
+      } else if (number2 > number3) {
+        console.log("number 2 is biggest!");
+      } else {
+        console.log("number 3 is biggest!");
+      }
+    }
+  };
+
+  const findWeekDays = () => {
+    let number = 7;
+    switch (number) {
+      case 1:
+        console.log("Monday");
+      case 2:
+        console.log("Tuesday");
+      case 3:
+        console.log("Wednesday");
+      case 4:
+        console.log("Thursday");
+      case 5:
+        console.log("Friday");
+      case 6:
+        console.log("Saturday");
+      case 7:
+        console.log("Sunday");
+    }
+  };
+
+  const getGrade = (score) => {
+    let grade;
+
+    switch (true) {
+      case score >= 90:
+        grade = "A";
+        break;
+      case score >= 80:
+        grade = "B";
+        break;
+      case score >= 70:
+        grade = "C";
+        break;
+      case score >= 60:
+        grade = "D";
+        break;
+      case score >= 50:
+        grade = "E";
+        break;
+      default:
+        grade = "F";
+    }
+
+    return grade;
+  };
+
+  const oddEven = (value) => {
+    return value % 2 == 0 ? "Even" : "Odd";
+  };
+
+  let number = -7;
+  negativePositive(number);
+
+  let age = 31;
+  isEligibleToVote(age);
+
+  findLargetst();
+
+  findWeekDays();
+
+  const score = 85;
+  const grade = getGrade(score);
+  console.log(`The grade for a score of ${score} is ${grade}.`);
+
+  const value = 6;
+  const answer = oddEven(value);
+  console.log(`The number ${value} is ${answer}`);
+};
+
+day3Learning();
