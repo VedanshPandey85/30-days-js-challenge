b5f8ecadeb50f741b6540ba5197cfa3231d8fabe
VedanshPandey85 <202312085@daiict.ac.in>
2024-07-28 00:21:16 +0530

    "updated day6 "

--- Day6.js
+++ Day6.js
@@ -30,9 +30,47 @@ const arrayIntermediate = () => {
 
   const doubleUsingMap = () => values.map((v) => v * 2);
   console.log("Doubled elements:- ", doubleUsingMap());
-  console.log(values);
+
+  const filterEvenVal = () => values.filter((x) => x % 2 == 0);
+  console.log("Even elements:- ", filterEvenVal());
+
+  const sumUsingReduce = () => values.reduce((a, b) => a + b, 0); // here zero is intial value
+  console.log("Sum of array :- ", sumUsingReduce());
+};
+
+const arrayIteration = () => {
+  let newArr = [3, 2, 1, 5, 6, 7];
+
+  const iteratingUsingForLoop = () => {
+    for (let index = 0; index < newArr.length; index++) {
+      console.log(newArr[index]);
+    }
+  };
+
+  console.log("Using for Each loop");
+  const forEachOverArray = () => {
+    newArr.forEach((element) => console.log(element));
+  };
+
+  iteratingUsingForLoop();
+  forEachOverArray();
+};
+
+const multiDimension = () => {
+  let matrix = [
+    [1, 2, 3],
+    [4, 5, 6],
+    [7, 8, 9],
+  ];
+  console.log(matrix);
+
+  for (let i = 0; i < matrix.length; i++)
+    for (let j = 0; j < matrix[i].length; j++)
+      console.log("element matrix[", i, "]", "[", j, "] -->", matrix[i][j]);
 };
 
 arrayCreation();
 arrayBasicMethods();
 arrayIntermediate();
+arrayIteration();
+multiDimension();
